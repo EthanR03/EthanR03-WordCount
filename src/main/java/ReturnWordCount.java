c8a0e70432c439c42e15cea6f216fec7b32df450
Ethan Rivera <ethanrivera206@yahoo.com>
2025-10-09 03:11:54 +0000

    Finished!!!

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -4,7 +4,11 @@ public class ReturnWordCount {
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+    public int count(String in) {
+
+        String[] words = in.trim().split("\\s+");
+        int count = words.length;
+
+        return count;
     }
 }
